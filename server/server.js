833aaa4ed0554abfb36f62f569751dd76d3301d9
dustinddoan <dustin.ddoan@gmail.com>
2023-04-12 18:28:29 -0700

    update port server

--- server/server.js
+++ server/server.js
@@ -49,5 +49,6 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
+    const port = server.address().port;
     console.log(`Server is running on port ${port}`);
 });
