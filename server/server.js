c086470181bb2db993a4ccc25d624c237381eb14
dustinddoan <dustin.ddoan@gmail.com>
2023-04-13 23:59:51 -0700

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -41,13 +41,13 @@ app.use((err, req, res, next) => {
 })
 
 // prepare for deployment
-// app.use(express.static('client/build'));
-// if (process.env.NODE_ENV === 'production') {
-//     const path = require('path');
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-//     })
-// }
+app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
